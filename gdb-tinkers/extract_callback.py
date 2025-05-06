28fbdeefab37216dcbe26f85c80ed0521dd5263e
Georgios Alexopoulos <grgalex@ba.uoa.gr>
2025-05-06 15:03:33 +0300

    extract
    Signed-off-by: Georgios Alexopoulos <grgalex@ba.uoa.gr>

--- gdb-tinkers/extract_callback.py
+++ gdb-tinkers/extract_callback.py
@@ -5,13 +5,20 @@ def addr2cb(address):
         gdb_address = gdb.parse_and_eval(f'({address})')
 
         # Use GDB's `info symbol` to get the symbol at the address
-        symbol_info = gdb.execute(f"set $x = *('v8::internal::JSObject' *){gdb_address}", to_string=True).strip()
+        set_x = gdb.execute(f"set $x = *('v8::internal::JSObject' *){gdb_address}", to_string=True).strip()
+        print(f'Result of set x : {set_x}')
 
         x_printed = gdb.execute(f"print $x", to_string=True).strip()
-        print(x_printed)
+        print(f'Result of print x : {x_printed}')
 
-        job_x = gdb.execute(f"job $x", to_string=True).strip()
+        job_x = gdb.execute(f"set $y =  _v8_internal_Print_Object_To_String((void*)($x))", to_string=True).strip()
+        y_printed = gdb.execute(f"print $y", to_string=True).strip()
+        print(f'Result of print y : {y_printed}')
+        # job_x = gdb.execute(f"job $x", to_string=True)
         print(job_x)
+        # print(f'Result of print job : {job_x}')
+
+        print('AFTER JOB')
 
         # if symbol_info:
         #     print(f'___ADDRESS___{address}___ADDRESS______FUNC___{symbol_info}___FUNC___')
@@ -21,5 +28,5 @@ def addr2cb(address):
         print(f'Error {e}')
 
 gdb.execute('set print demangle off')
-addr2cb(140727662896912)
-gdb.execute('quit')
+addr2cb(140725615296656)
+# gdb.execute('quit')
