dcdb053a0bdfe19e73064141d38cec2a8ff82e84
Georgios Alexopoulos <grgalex@ba.uoa.gr>
2025-05-15 17:25:38 +0300

    works deno_jid
    Signed-off-by: Georgios Alexopoulos <grgalex@ba.uoa.gr>

--- jid-1/native.cc
+++ jid-1/native.cc
@@ -1,18 +1,53 @@
 #include <napi.h>
 #include <iostream>
 #include <v8.h>
+
 namespace node {
 	namespace v8_utils {
-		void deno_jid(void *obj);
+		std::string deno_jid(void *obj);
+        void jid(const v8::FunctionCallbackInfo<v8::Value>& args);
 	}
 }
+
+class CallbackInfoPublic {
+public:
+      CallbackInfoPublic(napi_env env, napi_callback_info info);
+      ~CallbackInfoPublic();
+
+      Napi::Env Env() const;
+      Napi::Value NewTarget() const;
+      bool IsConstructCall() const;
+      size_t Length() const;
+      const Napi::Value operator[](size_t index) const;
+      Napi::Value This() const;
+      void* Data() const;
+      void SetData(void* data);
+      explicit operator napi_callback_info() const;
+
+public:  // everything below also public now
+      const size_t _staticArgCount = 6;
+      napi_env _env;
+      napi_callback_info _info;
+      napi_value _this;
+      size_t _argc;
+      napi_value* _argv;
+      napi_value _staticArgs[6]{};
+      napi_value* _dynamicArgs;
+      void* _data;
+};
+
 Napi::Value jid2(const Napi::CallbackInfo& info) {
 	Napi::Env env = info.Env();
 	std::string ret;
-	// auto x = *(info._argv);
-	// y = *(void **)x;
-	// ret = node::v8_utils::deno_jid(y);
-	ret = "hello";
+
+    void *y;
+
+    auto new_info = (CallbackInfoPublic&)(info);
+	auto x = *(new_info._argv);
+	y = *(void **)x;
+	ret = node::v8_utils::deno_jid(y);
+	// node::v8_utils::jid(argz);
+	// ret = "hello";
 	return Napi::String::New(env, ret);
 }
 
