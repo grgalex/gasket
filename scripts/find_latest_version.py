40b3574a47ebe3085149e71cdad0a7aed0737216
Georgios Alexopoulos <grgalex@ba.uoa.gr>
2025-05-22 00:57:23 +0300

    fib
    Signed-off-by: Georgios Alexopoulos <grgalex@ba.uoa.gr>

--- scripts/find_latest_version.py
+++ scripts/find_latest_version.py
@@ -1,3 +1,5 @@
+import time
+import json
 import argparse
 
 import utils
@@ -27,15 +29,29 @@ def main():
         sys.exit(1)
     package_names = utils.load_csv(args.input)
     pkg2ver = {}
+    final = []
 
     for pkg in package_names:
+        time.sleep(0.5)
         cmd = ['npm', 'show', pkg, 'version']
         try:
             ret, out, err = utils.run_cmd(cmd)
         except Exception as e:
             print(e)
+            continue
         ver = out.strip()
+        pkg2ver[pkg] =
         print(f"{pkg}:{ver}")
+
+    for pkg, ver in pkg2ver.items():
+        pkgver = pkg + ':' + ver
+        final.append(pkgver)
+    if args.output is not None:
+        with open(args.output, 'w') as outfile:
+            for pkgver in final:
+                outfile.write(f"{pkgver}\n")
+    else:
+        console.log(json.dumps(final, indent=2))
 
 if __name__ == "__main__":
     main()
