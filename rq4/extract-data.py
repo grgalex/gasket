10776f323824ba98c507311c23fc1c662d9dea3d
Georgios Alexopoulos <grgalex@ba.uoa.gr>
2025-05-31 19:30:31 +0300

    bz
    Signed-off-by: Georgios Alexopoulos <grgalex@ba.uoa.gr>

--- rq4/extract-data.py
+++ rq4/extract-data.py
@@ -90,7 +90,7 @@ def gen_histogram(data1, data2, histogram_filename):
     plt.gca().spines['bottom'].set_visible(False)  # Remove bottom spine
 
     # plt.xlim(0, max(data1))  # Limit x-axis to the range of the samples
-    plt.ylim(0, np.max(np.histogram(data1, bins=bins)[0]))  # Limit y-axis to the maximum frequency
+    # plt.ylim(0, np.max(np.histogram(data1, bins=bins)[0]))  # Limit y-axis to the maximum frequency
     # Title and axis labels
     # ax.set_title('Overlapping Histograms (Purple Overlap)')
     ax.set_xlabel('# Bridges found')
