0235cf275be3e4ef138b12fbe4995d65f3c4c6d4
Georgios Alexopoulos <grgalex@ba.uoa.gr>
2025-09-14 01:18:47 +0300

    we
    Signed-off-by: Georgios Alexopoulos <grgalex@ba.uoa.gr>

--- scripts/analyze_deno.js
+++ scripts/analyze_deno.js
@@ -4,6 +4,7 @@ import { hideBin } from 'npm:yargs/helpers'
 import * as fs from 'node:fs'
 import * as os from 'node:os'
 import * as path from 'node:path'
+import {randomUUID}  from 'node:crypto'
 
 const yargs = yargz(hideBin(process.argv))
 
